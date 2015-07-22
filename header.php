f93cd904f8c9b27cb362dbbc3ff04477206acfec
bennettscience <brian.bennett2@gmail.com>
2015-07-22 14:32:47 -0600

    Theme outline, no style applied.

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WriteSpace for WordPress</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_uri'); ?>" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <h1>HEADING</h1>
+      </div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div id="main">
+  <div ="content">
+    <h1>Main area</h1>
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+      <h4>Postd on <?php the_time( 'F jS, Y') ?></h4>
+      <p>
+        <?php the_content(__('(more...)')); ?>
+      </p>
+      <hr><?php endwhile; else: ?>
+        <p>
+          <?php _e('Sorry, no posts matched your criteria.'); ?>
+        </p><?php endif; ?>
+  </div>
+  <div id = "delimiter">
+  </div>
+  <?php get_footer( ); ?>
+</div>
