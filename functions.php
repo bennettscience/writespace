32ff46810c8579e6a87f33421796b64930729f42
bennettscience <brian.bennett2@gmail.com>
2015-07-24 14:52:23 -0400

    Remove header image default args

--- functions.php
+++ functions.php
@@ -5,4 +5,11 @@ function custom_menu() {
   register_nav_menu('header-menu',__( 'Header Menu'));
 }
 add_action( 'init', 'custom_menu');
+
+// Custom header image
+$args = array(
+  'default-image' => '',
+  'uploads'       => true,
+);
+add_theme_support('custom-header', $args );
 ?>
