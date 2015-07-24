ca94a5af14f005085a9c9b903f11920f49898a72
bennettscience <brian.bennett2@gmail.com>
2015-07-24 14:15:10 -0400

    +menu, CSS3 reset, basic menu styles

--- header.php
+++ header.php
@@ -3,10 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>WriteSpace for WordPress</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_uri'); ?>" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" charset="utf-8">
   </head>
   <body>
     <div id="wrapper">
       <div id="header">
-        <h1>HEADING</h1>
+        <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
       </div>
+      <hr />
+      <div id="nav">
+        <nav>
+          <?php wp_nav_menu(); ?>
+        </nav>
+      </div>
+      <hr />
