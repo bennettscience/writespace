19b39b9efbdd5eeadffb119227cde1a8b24add86
bennettscience <brian.bennett2@gmail.com>
2015-07-25 00:07:31 -0400

    Set full width images
    `.size-full` class now stretches outside the container div
    using a tweak to the margins of the image.
    
    Functions updated to remove `<p>` tag from image uploads.

--- functions.php
+++ functions.php
@@ -9,4 +9,16 @@ add_action( 'init', 'custom_menu');
 // Post featured image
 add_theme_support( 'post-thumbnails' );
 
+// Remove paragraph tag from images
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'filter_ptags_on_images');
+
+// Set default image link to 'none'
+// This allows the previous function to remove the tags correctly.
+// This is also necessary to ensure body images are full width.
+
+update_option('image_default_link_type', 'none');
+
 ?>
