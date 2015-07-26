a0e27b8e3f8bccbbaba4b93d1df1955b579190c5
bennettscience <brian.bennett2@gmail.com>
2015-07-26 16:09:20 -0400

    Add single post loop
    Loop through singe posts and display full content.

--- index.php
+++ index.php
@@ -1,30 +1,66 @@
 <?php get_header(); ?>
 <div id="main">
   <div ="post-content">
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+    <?php if ( is_home() || is_archive() ) { // start home loop ?>
+
+      <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
       <div class="the-post">
-      <?php if (has_post_thumbnail() ) {
-        the_post_thumbnail();
-      }
-      ?>
+        <?php if (has_post_thumbnail() ) {
+          the_post_thumbnail();
+        } ?>
+
       <div class="post-title">
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <p><?php the_time( 'F jS, Y') ?></p>
-      </div>
-      <div class="the-post-content">
+      </div> <!-- /post-title -->
+
+      <div class="the-post-content"><!-- if on the home page, display excerpt-->
         <p>
-          <?php the_content(__('(more...)')); ?>
+          <?php the_excerpt(__('more...')); ?>
         </p>
-      </div>
+      </div><!--/the-post-content"-->
+
       <div class="post-metadata">
         <p><?php the_category( ', ' ); ?></p>
         <?php the_tags( 'Tagged: '); ?></p>
-      </div>
+      </div><!--/post-metadata-->
+
       <hr>
-      <?php endwhile; else: ?>
+
+      <?php endwhile; else: ?><!-- If no posts available...-->
         <p>
           <?php _e('Sorry, no posts matched your criteria.'); ?>
-        </p><?php endif; ?>
+        </p>
+      <?php endif; ?>
+    <?php } // end home loop ?>
+
+    <?php if(is_single()) {  // start single loop ?>
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+          <div class="the-post">
+            <?php if (has_post_thumbnail() ) {
+              the_post_thumbnail();
+            } ?>
+
+          <div class="post-title">
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <p><?php the_time( 'F jS, Y') ?></p>
+          </div> <!-- /post-title -->
+
+          <div class="the-post-content"><!-- if on the home page, display excerpt-->
+            <p>
+              <?php the_content(); ?>
+            </p>
+          </div><!--/the-post-content"-->
+
+          <div class="post-metadata">
+            <p><?php the_category( ', ' ); ?></p>
+            <?php the_tags( 'Tagged: '); ?></p>
+          </div><!--/post-metadata-->
+
+        <?php endwhile; ?>
+          <?php endif; ?>
+        <?php } //end single loop ?>
   </div>
   <div id = "delimiter">
   </div>
