bfe1a2a70a7006eb12493f1c1589c6115413801e
bennettscience <brian.bennett2@gmail.com>
2015-08-13 21:38:24 -0400

    +page loop
    Added the page loop to index.php. Also added new container div for post thumbnails to make styling more flexible.
    Page header images are cropped down to 200px high.

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
       <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
       <div class="the-post">
-        <?php if (has_post_thumbnail() ) {
-          the_post_thumbnail();
-        } ?>
+        <div class="the-post-thumbnail">
+          <?php if (has_post_thumbnail() ) {
+            the_post_thumbnail();
+          } ?>
+        </div>
 
       <div class="post-title">
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
@@ -38,9 +40,11 @@
     <?php if(is_single()) {  // start single loop ?>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
           <div class="the-post">
-            <?php if (has_post_thumbnail() ) {
-              the_post_thumbnail();
-            } ?>
+            <div class="the-post-thumbnail">
+              <?php if (has_post_thumbnail() ) {
+                the_post_thumbnail();
+              } ?>
+            </div>
 
           <div class="post-title">
             <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
@@ -61,6 +65,30 @@
         <?php endwhile; ?>
           <?php endif; ?>
         <?php } //end single loop ?>
+
+        <?php if(is_page()) { // start page loop ?>
+          <?php if(have_posts()) : ?>
+            <?php while( have_posts()) : the_post(); ?>
+            <div class="the-post">
+              <div class="the-page-thumbnail">
+                <?php if(has_post_thumbnail() ) {
+                  the_post_thumbnail();
+                } ?>
+              </div>
+
+              <div class="post-title">
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              </div> <!-- /post-title -->
+
+              <div class="the-post-content"><!-- if on the home page, display excerpt-->
+                <p>
+                  <?php the_content(); ?>
+                </p>
+              </div><!--/the-post-content for pages-->
+            </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      <?php } //end page loop ?>
       </div>
       <div id = "delimiter"></div>
       <div id="footer">
